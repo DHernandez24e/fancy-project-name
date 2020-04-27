69b4b56cef8c5a847f24006cc604c2a58f09007d
Doggo <dhernandez24e@hotmail.com>
2020-04-27 13:48:00 -0700

    fixed bio, added similar artist array

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,37 +5,61 @@ var testFunction = function() {
     //Prevent page from reloading
     event.preventDefault();
 
+    //Clear forms?
+    $("#sim-art-ul").clear("");
+
     //Set search term value to variable
     var artist = artistSearchEl.value;
 
     //Artist search API set to variable
-    var artistSearchApi = "http://ws.audioscrobbler.com/2.0/?method=artist.search&artist="
-     + artist + 
-    "&api_key=702756dd5f8715a1c44e2754d353c270&format=json";
+    var artistSearchApi = "https://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist="
+    + artist +
+    "&api_key=702756dd5f8715a1c44e2754d353c270&format=json"
 
     //Top tracks of artist searched, API set to variable
-    var topTracksApi = "http://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="
+    var topTracksApi = "https://ws.audioscrobbler.com/2.0/?method=artist.gettoptracks&artist="
      + artist + 
     "&api_key=702756dd5f8715a1c44e2754d353c270&format=json";
 
-    //Top search result brings up artist searched for, other search results are closest to search term
+    //Search result brings up artist, items in response include picture, bio, and similar artists
     fetch(artistSearchApi).then(function (response) {
         response.json().then(function (data) {
             console.log(data);
-            //First item in artist array is the exact match for search result
-            console.log(data.results.artistmatches.artist[0])
+            //Find another API for image search for the artist
+                //console.log(data.artist.image[1]["#text"]);
+            
+            var similarArtists = data.artist.similar.artist;
+
+            //Testting removing <a> from bio string
+            var bioString = data.artist.bio.summary;
+            //Bio summary is split into an array, separating the anchor tag from the summary.
+            var bioArr = bioString.split('<a');
+            
+            //Artist name implemented to page
+            $("#artist-name").text(data.artist.name);
+                // $("#artist-pic").attr("src", data.artist.image[1]["#text"])
+            $("#artist-bio").text(bioArr[0]);
+
+            for (var i = 0; i < similarArtists.length; i++) {
+                var simArt = data.artist.similar.artist[i].name;
+
+                $("<li>").text(simArt).appendTo("#sim-art-ul");
+
+                //$("#sim-art-ul")
+            }
         })
-    })
+    });
 
     //Top tracks search brings up top 40 tracks for each search, determine how many results we should display
     fetch(topTracksApi).then(function (response) {
         response.json().then(function(data) {
-            console.log(data);
-            console.log(data.toptracks.track)
+            // console.log(data);
+
+            // console.log(data.toptracks.track[0].name, data.toptracks.track[1].name)
         })
     })
     
-
+    $(artistSearchEl).val("");
 
 }
 
